4c50713cab1dda4b0326c65b494b2c841a9e7bd6
m124578n <m23568n@gmail.com>
2023-06-07 16:04:47 +0800

    tutorial 1

--- rest/backend/views.py
+++ rest/backend/views.py
@@ -1,32 +1,49 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, status
-from rest_framework import permissions
-from .serializers import UserSerializer, GroupSerializer
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from backend.models import Snippet
+from backend.serializers import SnippetSerializer
 
-
-class UserViewSet(viewsets.ModelViewSet):
+@csrf_exempt
+def snippet_list(request):
     """
-    API endpoint that allows users to be viewed or edited.
+    List all code snippets, or create a new snippet.
     """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
-    queryset = User.objects.all().order_by("-date_joined")
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    @action(methods=['get'], detail=False)
-    def test(self, request):
-        user = User.objects.filter(username='root')
-        serializer = UserSerializer(user, many=True, context={'request': request})
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class GroupViewSet(viewsets.ModelViewSet):
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+@csrf_exempt
+def snippet_detail(request, pk):
     """
-    API endpoint that allows groups to be viewed or edited.
+    Retrieve, update or delete a code snippet.
     """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
 
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
